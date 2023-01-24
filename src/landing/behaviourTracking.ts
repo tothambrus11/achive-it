7e045ea4d4896c69516d480f66311072cd733baa
BuildTools <unconfigured@null.spigotmc.org>
2023-01-24 17:27:20 +0100

    CHG: comments to code

--- src/landing/behaviourTracking.ts
+++ src/landing/behaviourTracking.ts
@@ -1,3 +1,4 @@
+// Init global statistics object
 export const trackingInfo = {
     clickCount: 0,
     sessionStarted: +Date.now(),
@@ -5,9 +6,12 @@ export const trackingInfo = {
     charactersTyped: 0,
 };
 
+// Init info
 export function initTrackingInfo(){
     resetTrackingInfo();
 }
+
+// Reset statistics
 export function resetTrackingInfo(){
     trackingInfo.clickCount = 0;
     trackingInfo.sessionStarted = +Date.now();
@@ -15,21 +19,29 @@ export function resetTrackingInfo(){
     trackingInfo.charactersTyped = 0;
 }
 
+// Count all clicks on the web page
 window.addEventListener("click", ()=>{
     trackingInfo.clickCount++;
 });
 
+// Get the sign-up form
 const form = document.querySelector<HTMLFormElement>('#form-sign-up');
+
+// Gather all the input type elements(input, select, textarea)
 const inputs: HTMLElement[] = new Array<HTMLElement>();
 form!.querySelectorAll('input').forEach(input => inputs.push(input));
 form!.querySelectorAll('select').forEach(input => inputs.push(input));
 form!.querySelectorAll('textarea').forEach(input => inputs.push(input));
+
+// For each element track key presses
 inputs.forEach(input => {
     const inputEl = input as unknown as (HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement);
 
     // @ts-ignore
     inputEl.addEventListener("keydown", (e: KeyboardEvent) => {
         trackingInfo.keypressCount++;
+
+        // Check if key-press was character
         if(e.key.length === 1)
             trackingInfo.charactersTyped++;
     });
