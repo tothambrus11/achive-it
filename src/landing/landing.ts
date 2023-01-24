d6ac25be4f87170bd0b2cab3d29cdb20e98147e7
BuildTools <unconfigured@null.spigotmc.org>
2023-01-24 14:50:18 +0100

    CHG: regex fixes ADD: notes

--- src/landing/landing.ts
+++ src/landing/landing.ts
@@ -65,54 +65,106 @@ setTimeout(() => {
 
 export default 10;
 
-const idRegex = /^([A-Z])([a-zA-Z0-9$&+,:;=?@#|'<>.-^*()%!]{3,10})([0-9$&+,:;=?@#|'<>.-^*()%!])$/g;
-const passRegex = /^([a-zA-Z0-9$&+,:;=?@#|'<>.-^*()%!]{12,})$/g;
-const nameRegex = /^([a-zA-Z].*?)$/g;
-const countryRegex = /^([a-zA-Z].*?)$/g;
-const zipCodeRegex = /^([0-9]{4})([A-Z]{2})$/g;
-const emailRegex = /^[a-zA-Z0-9.!#$%&’*+\/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/g;
+const inputFields: {[id: string]: InputField} = {
+    'id': {
+        regex: /^([A-Z])([a-zA-Z0-9$&+,:;=?@#|'<>.-^*()%!]{3,10})([0-9$&+,:;=?@#|'<>.-^*()%!])$/,
+        regexMessage: 'ID should start with a capital letter and end with a number or special character',
+        required: true
+    },
+    'password': {
+        regex: /^([a-zA-Z0-9$&+,:;=?@#|'<>.-^*()%!]{12,})$/,
+        regexMessage: 'Password is too short',
+        required: true
+    },
+    'name': {
+        regex: /^([a-zA-Z].*?)$/,
+        regexMessage: 'Name may only contain the alphabet',
+        required: true
+    },
+    'country': {
+        regex: /^([a-zA-Z].*?)$/,
+        regexMessage: 'Country may only contain the alphabet',
+        required: true
+    },
+    'zip': {
+        regex: /^([0-9]{4})([A-Z]{2})$/,
+        regexMessage: 'Zipcode does not follow format: 1234AB',
+        required: true
+    },
+    'email': {
+        regex: /^[a-zA-Z0-9.!#$%&’*+\/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+        regexMessage: 'Invalid email address',
+        required: true
+    },
+    'sex': {
+        regex: /^[MFO]$/,
+        regexMessage: 'Choose an option',
+        required: true
+    }
+}
 
 document.querySelectorAll('form').forEach(formEl => {
     const form = formEl as HTMLFormElement;
     form.addEventListener('submit', e => {
-        let pass: boolean = true;
-        form.querySelectorAll<HTMLInputElement>('input').forEach(input => {
-            let inputEl = input as HTMLInputElement;
-            let regex: RegExp | null;
-            switch (inputEl.name) {
-                case 'id':
-                    regex = idRegex;
-                    break;
-                case 'pass':
-                    regex = passRegex;
-                    break;
-                case 'name':
-                    regex = nameRegex;
-                    break;
-                case 'zip':
-                    regex = zipCodeRegex;
-                    break;
-                case 'email':
-                    regex = emailRegex;
-                    break;
-                case 'country':
-                    regex = countryRegex;
-                    break;
-                default:
-                    regex = null;
-                    break;
-            }
+        e.preventDefault();
 
-            if(!regex)
+        let inputs: HTMLElement[] = new Array<HTMLElement>();
+        form.querySelectorAll('input').forEach(input => inputs.push(input));
+        form.querySelectorAll('select').forEach(input => inputs.push(input));
+        form.querySelectorAll('textarea').forEach(input => inputs.push(input));
+
+        inputs.forEach(input => {
+            const inputEl = input as unknown as (HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement);
+            let inputField = inputFields[inputEl.name];
+
+            let note = getNote(inputEl, inputEl.name);
+
+            if(!inputField) {
+                success(note);
                 return;
-
-            if(!regex.test(inputEl.value)) {
-                console.log(inputEl.name, 'failed');
-                pass = false;
             }
-        });
 
-        if(!pass)
-            e.preventDefault();
+            if(inputField.required && !inputEl.value) {
+                error(note, 'This is a required field');
+                return;
+            }
+
+            if(!inputField.regex || inputField.regex.test(inputEl.value)) {
+                success(note);
+                return;
+            }
+
+            error(note, inputField.regexMessage ? inputField.regexMessage : 'Please provide a valid '+inputEl.name);
+        });
     });
 });
+
+function getNote(input: HTMLElement, id: string): HTMLSpanElement {
+    let note = input.parentElement!.querySelector('#note-'+id) as HTMLSpanElement;
+    if(!note) {
+        note = document.createElement('span');
+        note.setAttribute('id', 'note-'+id);
+        note.classList.add('input-note');
+        input.parentNode!.insertBefore(note, input.nextSibling);
+    }
+
+    return note;
+}
+
+function success(note: HTMLSpanElement): void {
+    note.style.color = '#669B45';
+    note.innerHTML = 'Looks good!'
+}
+
+function error(note: HTMLSpanElement, message: string): void {
+    note.style.color = '#B8695F';
+    note.innerText = message;
+}
+
+interface InputField {
+    regex: RegExp | null;
+    
+    regexMessage?: string;
+    required: boolean;
+
+}
